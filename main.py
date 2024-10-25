b9ca728df7c0efe9de92fc9f9884a22ecfc08630
vepaan <141347566+vepaan@users.noreply.github.com>
2024-10-24 23:28:57 -0500

    Playing with libraries

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import numpy
+from statsmodels.regression.rolling import RollingOLS
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+import datetime as dt
+import yfinance as yf
+import pandas_ta
+import warnings
+warnings.filterwarnings('ignore')
 
-print(numpy.mean([1,2,3,4]))
+a = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+print(a)
