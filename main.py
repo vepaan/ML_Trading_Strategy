f5563a2d784ee43c009d814e2b7ff545cea81d0c
vepaan <141347566+vepaan@users.noreply.github.com>
2024-11-01 23:16:34 -0500

    just some indicators

--- main.py
+++ main.py
@@ -10,5 +10,17 @@ import pandas_ta
 import warnings
 warnings.filterwarnings('ignore')
 
-a = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-print(a)
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+sp500['Symbol'] = sp500['Symbol'].str.replace('.', '-')
+symbols_list = sp500['Symbol'].unique().tolist()
+
+end_date = dt.datetime.now()
+start_date = pd.to_datetime(end_date) - pd.DateOffset(365*8)
+
+df = yf.download(tickers = symbols_list, start = start_date, end = end_date)
+df = df.stack()
+df.index.names = ['date', 'ticker']
+df.columns = df.columns.str.lower()
+print(df)
+
+
